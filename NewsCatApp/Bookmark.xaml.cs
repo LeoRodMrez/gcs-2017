2075e53f42105a24d2c4371f2ae64bc7ed734f41
youchenz <youchen_you@hotmail.com>
2017-03-06 13:43:19 +0100

    Añadido pantallas Article, Bookmark, Favorites y Feed

---
+++ NewsCatApp/Bookmark.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NewsCatApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Bookmark : ContentPage
+    {
+        public Bookmark()
+        {
+            InitializeComponent();
+        }
+        private void Perfil_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Perfil());
+        }
+
+        private void Leer_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Article());
+        }
+    }
+}
---
+++ NewsCatApp/Favorites.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NewsCatApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Favorites : ContentPage
+    {
+        public Favorites()
+        {
+            InitializeComponent();
+        }
+
+        private void Perfil_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Perfil());
+        }
+
+        private void Leer_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Article());
+        }
+    }
+}
